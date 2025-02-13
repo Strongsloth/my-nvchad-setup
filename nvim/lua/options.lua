a521a50fe7ea599a286247858fbd2ae78647b06f
Sloth.dev <cabalitgu1@gmail.com>
2025-02-13 21:23:05 +0800

    Add files via upload

---
+++ nvim/lua/options.lua
@@ -0,0 +1,18 @@
+require "nvchad.options"
+
+
+local opt = vim.opt
+local o = vim.o
+local wo = vim.wo
+
+opt.relativenumber = true
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.softtabstop = 4
+opt.expandtab = true
+
+o.cursorlineopt = "both"
+o.shell = "pwsh"
+
+wo.number = true
+wo.relativenumber = true
